cd3544c6ac811cd95aaba3a9616c2380e97c95d9
mercyx <90040892+realmercy@users.noreply.github.com>
2022-02-12 04:36:57 +0000

    fuck me

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 const Discord = require("discord.js")
-
-const client = new Discord.Client({
-
-  intents: 32767
-
+const fs = require("fs")
+const client = new Discord.Client({ intents: 32767
 })
-
 require("dotenv").config()
-
-
+const prefix = "$"
 client.on('ready', () => {
-
-  console.log(`${client.user.tag} is online in ${client.guilds.size.cache} servers les gooo`)
-
+ console.log(`${client.user.tag} is online in ${client.guilds.cache.size} servers les gooo`)
 })
-
-
 client.login(process.env.token)
 
